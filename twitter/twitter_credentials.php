509560f02db4a39bc65813b8b734706d60038431
JeesooKim <jeesoo.joa@gmail.com>
2015-06-10 12:51:30 -0400

    twitterUpdate

---
+++ twitter/twitter_credentials.php
@@ -0,0 +1,9 @@
+<?php
+
+
+define( 'CONSUMER_KEY' , 'Pd0XtdLdBQT8zhJwPC8AIcwxT');
+define( 'CONSUMER_SECRET' , 's2IsOaRhq6OzXn9swLk5mpUL6ZlWRxk7k5G3AzvSAOrjCY86ic');
+define( 'ACCESS_TOKEN' , '224515093-fEmNTRhqBsyyUvhjj4lcUI8xM4NRxScs56eWpZik');
+define( 'ACCESS_TOKEN_SECRET' , '06KruIWbMAQiBMsWFxltt8BPLRCqb0W0c76Y6gvAHrUjG');
+
+?>
