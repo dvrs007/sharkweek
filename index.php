089a650a09dc00e34504d392e46e3dd1095fac16
johnsonta87 <johnsonta87@gmail.com>
2015-06-10 11:31:22 -0400

    johnson- adding foundations

--- index.php
+++ index.php
@@ -8,6 +8,14 @@
         <script src="js/foundation.min.js" type="text/javascript" ></script>
     </head>
     <body>
-        
+        <div class="medium-4 columns">
+            <h1>LinkedIn</h1>
+        </div>
+        <div class="medium-4 columns">
+            <h1>Vine</h1>
+        </div>
+        <div class="medium-4 columns">
+            <h1>Instagram</h1>
+        </div>
     </body>
 </html>
