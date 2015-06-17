b9a77a73460e0582042d462dae776cab134d2580
JeesooKim <jeesoo.joa@gmail.com>
2015-06-16 22:17:46 -0400

    vine_debuggING

--- vine_results.php
+++ vine_results.php
@@ -1,5 +1,5 @@
 <?php
-require 'model/vine_Class.php';
+//require 'model/vine_Class.php';
 //require 'model/database.php';
 
 
@@ -12,7 +12,24 @@ function pre( $data)
 	
 }
 
-$search = file_get_contents('https://api.vineapp.com/timelines/tags/sharkweek');
+function file_get_contents_curl($url) {    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    $data = curl_exec($ch);
+
+    curl_close($ch);
+    return $data;
+}
+
+
+print_r( file('https://api.vineapp.com/timelines/tags/sharkweek'));
+
+
+
+$search = file_get_contents_curl('https://api.vineapp.com/timelines/tags/sharkweek');
 
 //convert to json and display as an array
 $search = json_decode($search, true);
@@ -21,12 +38,13 @@ echo '<h1>-----------------VINE starts----------------</h1>';
 pre($search);
 echo '<h1>-----------------VINE ends----------------</h1>';
 
+/*
+
 foreach($search['data']['records'] as $key => $value)
 {
    $embed = file_get_contents('https://vine.co/oembed.json?url='.urlencode($value['shareUrl']));
 
-    $embed = json_decode($embed,true);
-    
+    $embed = json_decode($embed,true);  
     
         //new instance of vine class
         $vine = new VineClass;
@@ -37,8 +55,7 @@ foreach($search['data']['records'] as $key => $value)
         $vine->insertVine();
         
         //var_dump($vine);
-    
 }
 
-
+     */
 ?>
